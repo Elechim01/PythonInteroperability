adf03237fbf42a7f42df6a6a82595b86e626435e
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2022-10-01 11:52:15 +0200

    Initial Commit

---
+++ PythonInteroperability/PythonInteroperabilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  PythonInteroperabilityApp.swift
+//  PythonInteroperability
+//
+//  Created by Michele Manniello on 01/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PythonInteroperabilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
